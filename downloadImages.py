83e8e2f10cbd6d89df525b892c90ff265de3d092
Victor Guyard <19635051+FlipFloop@users.noreply.github.com>
2023-11-07 11:27:06 -0500

    Images

--- downloadImages.py
+++ downloadImages.py
@@ -16,5 +16,10 @@ for price, urls in data.items():
         # Extract the UUID from the URL
         image_uuid = url.split('t_product_v1/')[-1].split("/")[0].split(",")[0]
         
-        # Download the image and save it in the corresponding directory
-        urllib.request.urlretrieve(url, os.path.join("./dataset", price, f'{image_uuid}.jpg'))
+        # Define the path where the image will be saved
+        image_path = os.path.join("./dataset", price, f'{image_uuid}.jpg')
+        
+        # Check if the image already exists
+        if not os.path.exists(image_path):
+            # Download the image and save it in the corresponding directory
+            urllib.request.urlretrieve(url, image_path)

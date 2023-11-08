b4b8176e5842f994a614fab0bfb48b9c5be37a9a
Victor Guyard <19635051+FlipFloop@users.noreply.github.com>
2023-11-08 11:30:54 -0500

    Reebok!

--- imageVerify.py
+++ imageVerify.py
@@ -5,11 +5,15 @@ dataset_folder = './dataset'
 
 imageTypes = [".jpg", ".jpeg", ".png"]
 
+image_count = 0
+
 for filename in os.listdir(dataset_folder):
     if filename.endswith(tuple(imageTypes)):
         try:
             img = Image.open(os.path.join(dataset_folder, filename)) 
             img.verify()
+            image_count += 1
+            print(f"Checked {image_count} images")
         except (IOError, SyntaxError) as e:
             print('Bad file:', filename)
             os.remove(os.path.join(dataset_folder, filename))

ac7b33b233248ed9e522c1f58962578dddc8298c
Victor Guyard <19635051+FlipFloop@users.noreply.github.com>
2023-11-09 17:46:48 -0500

    More Adidas

--- countImgs.py
+++ countImgs.py
@@ -6,11 +6,13 @@ scraped_data_folder = 'scrapedData'
 
 for filename in os.listdir(scraped_data_folder):
     if filename.endswith('.json'):
+        count_file = 0
         file_path = os.path.join(scraped_data_folder, filename)
         with open(file_path) as f:
             data = json.load(f)
             for key in data:
                 count += len(data[key])
-        print(f"Total number of URLs in {filename}", count)
+                count_file += len(data[key])
+        print(f"Total number of URLs in {filename}", count_file)
 
 print("Total number of URLs: ", count)
